a9c800e30175acd9a7b18b078ea3130be72bc900
Sebastian <sebaj348@gmail.com>
2026-04-29 20:57:08 -0400

    fix principal

--- src/controllers/Cl_cMedico.ts
+++ src/controllers/Cl_cMedico.ts
@@ -3,14 +3,16 @@ import Cl_mMedico from "../models/Cl_mMedico.js";
 import Cl_cPaciente from "./Cl_cPaciente.js";
 
 export default class Cl_cMedico {
-  private mMedico:Cl_mMedico =  new Cl_mMedico();
+  private mMedico:Cl_mMedico;
   private vMedico: I_vMedico;
   private cPaciente: Cl_cPaciente;
 
   constructor(
+    mMedico: Cl_mMedico,
     vMedico: I_vMedico,
     cPaciente: Cl_cPaciente,
   ) {
+    this.mMedico = mMedico;
     this.vMedico = vMedico;
     this.cPaciente = cPaciente;
     this.vMedico.onNuevoPaciente(() => this.procesarUnPaciente());
